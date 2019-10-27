ec1355ea4d8dd0403103c91743eab01a841689ce
Anderson Silva <andersoonn.ams@gmail.com>
2019-10-27 17:44:22 -0300

    Add routes

---
+++ routes/feed.js
@@ -0,0 +1,107 @@
+const { Feed, validate } = require('../models/feed')
+const auth = require('../middleware/auth')
+const admin = require('../middleware/admin')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  // The first 4 bytes are the timestamp
+  const feed = await Feed.find({isAuthorized: true}).sort('_id')
+  res.send(feed)
+})
+
+
+router.get('/:id', async (req, res) => {
+
+  const feed = await Feed.findById(req.params.id)
+
+  if(!feed) return res.status(404).send('The item of the give ID was not found.')
+  if(!feed.isAuthorized) return res.status(403).send('Access Denied.')
+
+  res.send(feed)
+})
+
+router.post('/', auth, async (req, res) => {
+  
+  const { error } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+
+  let feed = new Feed({
+    title: req.body.title,
+    description: req.body.description,
+    isAnon: req.body.isAnon,
+    author: req.user._id,
+    isAuthorized: false
+
+  })
+
+  feed = await feed.save()
+  res.send(feed)
+})
+
+router.put('/:id', auth, async (req, res) => {
+  const { error } = validate(req.body)
+
+  if(error) return res.status(400).send(error.details[0].message)
+
+  let feed = await Feed.findOne({_id : req.params.id})
+
+  if(!feed) return res.status(404).send('The item of the given ID was not found.')
+
+  if(feed.author != req.user._id) return res.status(403).send('Access Denied.')
+
+  feed.title = req.body.title 
+  feed.description = req.body.description
+  feed.isAnon = req.body.isAnon
+
+
+  feed = await feed.save()
+
+  /*
+  const feed = await Feed.findByIdAndUpdate(req.params.id,
+    {
+      title: req.body.title,
+      description: req.body.description,
+      isAnon: req.body.isAnon,
+      author: req.body.author,
+      isAuthorized: false
+    }, { new: true })
+  */
+
+  res.send(feed)
+})
+
+router.delete('/:id', auth, async (req, res) => {
+
+  let feed = await Feed.findOne({_id: req.params.id})
+  
+  if(!feed) return res.status(404).send('The item of the given ID was not found.')
+  
+  if(req.user.isAdmin){
+    feed = await Feed.findByIdAndRemove(req.params.id);
+    res.send(feed)
+  }
+
+  if(!(feed.author == req.user._id)) return res.status(403).send('Access Denied.')
+  feed = await Feed.findByIdAndRemove(req.params.id);
+  res.send(feed)
+})
+
+
+router.post('/authorize/:id', auth, admin, async(req, res) => {
+  let feed = await Feed.findOne({_id: req.params.id})
+
+  if(!feed) return res.status(404).send('The item of the given ID was not found.')
+
+  feed.isAuthorized = true
+
+  feed = await feed.save()
+
+  return res.send(feed)
+
+})
+
+
+
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,77 @@
-const express = require("express");
+const { User, validate } = require('../models/user')
+const auth = require('../middleware/auth')
+const _ = require('lodash')
+const express = require('express')
+const bcrypt = require('bcrypt')
+const config = require('config')
+
+const {google} = require('googleapis')
+
 const router = express.Router();
-const { User, validate } = require("../models/user");
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (ex) {
-    console.log(ex.message);
-  }
-});
 
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User({
-    name: req.body.name
-  });
+router.get('/oauth/google', (req, res) => {
 
-  user.save();
+  
+  const oauth2Client = new google.auth.OAuth2(
+    config.get('clientId'),
+    config.get('clientSecret'),
+    'http://localhost:3000/api/users/oauth/google/login'
+  );
 
-  res.send(user);
-});
+  
+  const url = oauth2Client.generateAuthUrl({
+    acces_type: 'offline',
+    prompt: 'consent',
+    scope: ['https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/userinfo.email']
+  })
 
-module.exports = router;
+  res.send(url)
+
+})
+
+
+router.get('/oauth/google/login', (req, res) => {
+
+
+  const {tokens} =  oauth2Client.getToken(req.body.access_token)
+  console.log(tokens)
+  oauth2Client.setCredentials(tokens);
+  console.log(oauth2Client)
+
+  res.send(oauth2Client)
+
+})
+
+
+
+
+router.get('/me', auth, async (req, res) =>{
+  const user = await User.findById(req.user._id).select('-password')
+  res.send(user)
+})
+
+router.post('/', async (req, res) => {
+
+  const { error } = validate(req.body)
+  if (error ) return res.status(400).send(error.details[0].message)
+
+  let user = await User.findOne({ email: req.body.email })
+  if (user) return res.status(400).send('User already registered.')
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']))
+  user.isAdmin =  false
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
+
+  await user.save()
+
+  const token = user.generateAuthToken()
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
+
+})
+
+module.exports = router
+
