0acf5ec089466790725f815fc5bf6b1d02e53159
Anderson Silva <andersoonn.ams@gmail.com>
2019-11-06 20:51:19 -0300

    Add google auth

--- routes/users.js
+++ routes/users.js
@@ -56,6 +56,7 @@ router.get('/me', auth, async (req, res) =>{
 router.post('/', async (req, res) => {
 
   const { error } = validate(req.body)
+
   if (error ) return res.status(400).send(error.details[0].message)
 
   let user = await User.findOne({ email: req.body.email })
