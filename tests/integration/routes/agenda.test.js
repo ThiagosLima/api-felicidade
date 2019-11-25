3f76a6fd35be08d209fd11b1ebe3de3a42a0bd21
Thiago Lima <thiagolima.unb@gmail.com>
2019-11-25 20:29:06 -0300

    Add initial tests configuration

---
+++ tests/integration/routes/agenda.test.js
@@ -0,0 +1,37 @@
+// This is a exemple test
+const request = require("supertest");
+const { Agenda } = require("../../../models/agenda");
+const { User } = require("../../../models/user");
+
+let server;
+
+describe("index", () => {
+  beforeEach(() => {
+    server = require("../../../index");
+  });
+
+  afterEach(async () => {
+    server.close();
+    await Agenda.deleteMany({});
+  });
+
+  describe("GET /", () => {
+    it("should return all agendas", async () => {
+      await User.collection.insertMany([
+        {
+          name: "user1",
+          email: "user1@mail.com",
+          password: 12345
+        },
+        {
+          name: "user2",
+          email: "user2@mail.com",
+          password: 12345
+        }
+      ]);
+
+      const res = await request(server).get("/api/agenda");
+      expect(res.status).toBe(200);
+    });
+  });
+});
