ffa14e96ba3d225d07e6802fcc9f79031c17cd54
Thiago Lima <thiagolima.unb@gmail.com>
2019-12-01 08:49:52 -0300

    Fix validate

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,26 @@
-const Joi = require("@hapi/joi")
-const bcrypt = require("bcrypt")
-const _ = require("lodash")
-const { User, validate } = require("../models/user")
+const Joi = require('@hapi/joi')
+const bcrypt = require('bcrypt')
+const { User } = require('../models/user')
 
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
   const user = await User.findOne({ email: req.body.email })
 
-  if (!user) return res.status(400).send("Invalid email or password.")
+  if (!user) return res.status(400).send('Invalid email or password.')
 
   const validPassword = await bcrypt.compare(req.body.password, user.password)
-  if (!validPassword) return res.status(400).send("Invalid email or password.")
+  if (!validPassword) return res.status(400).send('Invalid email or password.')
 
   const token = user.generateAuthToken()
   res.send(token)
 })
 
-function validate(req) {
+function validate (req) {
   const schema = {
     email: Joi.string()
       .min(5)
